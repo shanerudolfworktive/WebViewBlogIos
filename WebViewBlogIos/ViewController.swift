46766d62d4751c460dcfc092829d69d0cbbb2127
Shane Rudolf <shane1@Shanes-MacBook-Pro.local>
2018-01-22 23:53:56 +0800

    first commit

--- WebViewBlogIos/ViewController.swift
+++ WebViewBlogIos/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import WebKit
+import JavaScriptCore
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
+    var webView : WKWebView? = nil
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let controller = WKUserContentController()
+        controller.add(self, name: "iosApp")
+        
+        let configuration = WKWebViewConfiguration()
+        configuration.userContentController = controller
+        
+        webView = WKWebView(frame: self.view.frame, configuration: configuration)
+        let url = Bundle.main.url(forResource: "WebViewBlog", withExtension: "html")
+        let request = URLRequest(url: url!)
+        self.view.addSubview(webView!)
+        webView?.load(request)
+        
+        webView?.navigationDelegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        print("Message from Javascript: \(message.body)")
+        let platform = UIDevice.current.model;
+        let osVersion = UIDevice.current.systemVersion;
+        webView?.evaluateJavaScript("renderOSVersion('\(platform) : \(osVersion)')", completionHandler: nil)
     }
-
-
 }
+
 
