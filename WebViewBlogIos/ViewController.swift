339315e9202811aab88554ddbf84b6c7fb9560a2
Shane Rudolf <shane1@Shanes-MacBook-Pro.local>
2018-01-25 21:37:57 +0800

    update text

--- WebViewBlogIos/ViewController.swift
+++ WebViewBlogIos/ViewController.swift
@@ -8,32 +8,30 @@
 
 import UIKit
 import WebKit
-import JavaScriptCore
 
-class ViewController: UIViewController, WKScriptMessageHandler, WKNavigationDelegate {
-    var webView : WKWebView? = nil
+class ViewController: UIViewController, WKScriptMessageHandler {
+    var webview : WKWebView! = nil
     override func viewDidLoad() {
         super.viewDidLoad()
-        let controller = WKUserContentController()
-        controller.add(self, name: "iosApp")
         
-        let configuration = WKWebViewConfiguration()
-        configuration.userContentController = controller
+        webview = WKWebView(frame: self.view.frame, configuration: WKWebViewConfiguration())
+        webview.configuration.userContentController.add(self, name: "iosApp")
         
-        webView = WKWebView(frame: self.view.frame, configuration: configuration)
         let url = Bundle.main.url(forResource: "WebViewBlog", withExtension: "html")
         let request = URLRequest(url: url!)
-        self.view.addSubview(webView!)
-        webView?.load(request)
-        
-        webView?.navigationDelegate = self
+        self.view.addSubview(webview)
+        webview.load(request)
     }
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         print("Message from Javascript: \(message.body)")
-        let platform = UIDevice.current.model;
-        let osVersion = UIDevice.current.systemVersion;
-        webView?.evaluateJavaScript("renderOSVersion('\(platform) : \(osVersion)')", completionHandler: nil)
+        let platform = UIDevice.current.model
+        let osVersion = UIDevice.current.systemVersion
+        invokeJavascript(message: "\(platform) : \(osVersion)")
+    }
+    
+    func invokeJavascript(message:String){
+        webview.evaluateJavaScript("render('\(message)')", completionHandler: nil)
     }
 }
 
